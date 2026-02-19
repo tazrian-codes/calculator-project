ddee1d5547bccd18c6b2a940ef0dc36bad718fa4
Tazrian <tazrianislam06@gmail.com>
2026-02-19 15:43:44 +0600

    Completed styling and logics

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ const inputBox = document.getElementById('inputBox');
 
 function buttonClick(p) {
   inputBox.value = inputBox.value+p;
+  inputBox.focus();
 }
 
 function dlt() {
   inputBox.value = inputBox.value.slice(0,-1);
+  inputBox.focus();
 }
 
 function clearAll() {
   inputBox.value = '';
+  inputBox.focus();
 }
 
 function result() {
   try{
     inputBox.value = eval(inputBox.value).toFixed(5);
+    inputBox.focus();
   }
   catch{
     alert('Please give a valid equation.');
+    inputBox.focus();
   }
 }
