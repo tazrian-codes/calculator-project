3363fbd986da2fa0e4e2fec2aa7b758bd59d6dba
Tazrian <tazrianislam06@gmail.com>
2026-02-19 15:46:24 +0600

    Added comments

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 // DOM elements
 const inputBox = document.getElementById('inputBox');
 
+// function that takes the value as a parameter and add it to the inputBox
 function buttonClick(p) {
   inputBox.value = inputBox.value+p;
   inputBox.focus();
 }
 
+// function which delete one digit at once
 function dlt() {
   inputBox.value = inputBox.value.slice(0,-1);
   inputBox.focus();
 }
 
+// function that clear the input box
 function clearAll() {
   inputBox.value = '';
   inputBox.focus();
 }
 
+// function that calculates the equation
 function result() {
   try{
     inputBox.value = eval(inputBox.value).toFixed(5);
